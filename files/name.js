c4bc694841a27cdafdb292aac6a5b6e94c62e8e2
xiaoyiling0 <3422709697@qq.com>
2024-01-13 16:58:12 +0800

    1

---
+++ files/name.js
@@ -0,0 +1,100 @@
+var pullname = function(i){
+	switch(Math.floor(Math.random()*48+1)){
+		case 1 : 
+ 			namearray[i]='包睿'; break;
+		case 2 : 
+ 			namearray[i]='陈瑜'; break;
+		case 3 : 
+ 			namearray[i]='段玉'; break;
+		case 4 : 
+ 			namearray[i]='姜珍怡'; break;
+		case 5 : 
+ 			namearray[i]='林思雨'; break;
+		case 6 : 
+ 			namearray[i]='任冉'; break;
+		case 7 : 
+ 			namearray[i]='王瑞泽'; break;
+		case 8 : 
+ 			namearray[i]='校睿'; break;
+		case 9 : 
+ 			namearray[i]='许昕奕'; break;
+		case 10 : 
+ 			namearray[i]='张润达'; break;
+		case 11 : 
+ 			namearray[i]='陈超'; break;
+		case 12 : 
+ 			namearray[i]='戴成浩'; break;
+		case 13 : 
+ 			namearray[i]='范睿涵'; break;
+		case 14 : 
+ 			namearray[i]='金定一'; break;
+		case 15 : 
+ 			namearray[i]='刘阳'; break;
+		case 16 : 
+ 			namearray[i]='孙国亮'; break;
+		case 17 : 
+ 			namearray[i]='王劭谦'; break;
+		case 18 : 
+ 			namearray[i]='徐耀林'; break;
+		case 19 : 
+ 			namearray[i]='杨世雨'; break;
+		case 20 : 
+ 			namearray[i]='赵晨曦'; break;
+		case 21 : 
+ 			namearray[i]='陈恩东'; break;
+		case 22 : 
+ 			namearray[i]='戴祺轩'; break;
+		case 23 : 
+ 			namearray[i]='韩欣睿'; break;
+		case 24 : 
+ 			namearray[i]='孔令坤'; break;
+		case 25 : 
+ 			namearray[i]='吕妍'; break;
+		case 26 : 
+ 			namearray[i]='孙亦陈'; break;
+		case 27 : 
+ 			namearray[i]='王智梁'; break;
+		case 28 : 
+ 			namearray[i]='徐英赫'; break;
+		case 29 : 
+ 			namearray[i]='姚曼文'; break;
+		case 30 : 
+ 			namearray[i]='周童'; break;
+		case 31 : 
+ 			namearray[i]='陈杰'; break;
+		case 32 : 
+ 			namearray[i]='邓岚予'; break;
+		case 33 : 
+ 			namearray[i]='洪一诺'; break;
+		case 34 : 
+ 			namearray[i]='李博仪'; break;
+		case 35 : 
+ 			namearray[i]='梅之谦'; break;
+		case 36 : 
+ 			namearray[i]='吴双'; break;
+		case 37 : 
+ 			namearray[i]='徐子寒'; break;
+		case 38 : 
+ 			namearray[i]='殷雨婷'; break;
+		case 39 : 
+ 			namearray[i]='周星如'; break;
+		case 40 : 
+ 			namearray[i]='陈优'; break;
+		case 41 : 
+ 			namearray[i]='窦悦然'; break;
+		case 42 : 
+ 			namearray[i]='胡恒'; break;
+		case 43 : 
+ 			namearray[i]='李一豪'; break;
+		case 44 : 
+ 			namearray[i]='闵忠奇'; break;
+		case 45 : 
+ 			namearray[i]='王鹏'; break;
+		case 46 : 
+ 			namearray[i]='肖文涛'; break;
+		case 47 : 
+ 			namearray[i]='许靖坤'; break;
+		case 48 : 
+ 			namearray[i]='张一然'; break;
+	};
+};
--- files/textjs.js
+++ files/textjs.js
@@ -1,35 +1,50 @@
 var video = document.getElementsByTagName('video');
 var buttons = document.getElementsByTagName('button');
-//var n =
+var namearray =new Array()
+var ne = document.getElementById('newelement');
+var n 
+
 buttons[0].onclick = function(){		//进入流星视频
+	n = 1;
+	for(i=0;i<n;i++){
+		pullname(i);
+	};
 	video[0].src = "./video/3star-single.mp4";
 	video[0].style.zIndex = 2;
 	video[0].loop = false;
 	video[0].muted = false;
+	var skip = ne.appendChild(document.createElement('button'))		//跳过进入result
+	skip.appendChild(document.createTextNode('跳过▶'))
+	skip.setAttribute('class','skipbutton')
+	skip.onclick = function(){
+		ne.removeChild(skip);
+		result(n);
+	};
 };
 
-video[0].addEventListener('ended',function(){
-	if(video[0].style.zIndex = 2){
-		restore();
-		document.write('<img src="./image/splash-background-49b61db1.webp" width:100%;height:100%;object-fit:cover;position:absolute;top:0;left:0;z-index:2>');
+video[0].addEventListener('ended',function(){		//视频播放完毕进入result
+	if(video[0].style.zIndex == 2){	
+		result()
+	
 	}
-});
+});	
 
-var restore =function(){
-	video[0].src = "./video/bg.webm";
+
+var result = function(){
+	var nb = ne.appendChild(document.createElement('img'))		//result背景图片
+	nb.setAttribute('src','./image/splash-background-49b61db1.webp');
+	nb.setAttribute('class','nb');
+	video[0].src = "./video/bg.webm";		//背景视频恢复
 	video[0].style.zIndex = -1;
 	video[0].loop = true;
 	video[0].muted = true;
+	var nn=ne.appendChild(document.createElement('span'));		//名字、背景、叉号按钮出现
+	nn.appendChild(document.createTextNode(namearray[0]));
+	nn.setAttribute('class','mainname')
+	buttons[2].style.display='inline';
 }
 
-
-
-
-
-//buttons[1].onclick = function(){
-//	liuxing(n)
-//};
-
-
-
-		
+buttons[2].onclick =function(){
+	ne.innerHTML = "";
+	buttons[2].style.display='none';
+}
