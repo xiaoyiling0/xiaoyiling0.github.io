9649300e2f4038d3a6c6ecc054a69465ba35f92b
萧翊凌xyl <154736513+xiaoyiling0@users.noreply.github.com>
2024-01-14 20:42:09 +0800

    Update name.js

--- files/name.js
+++ files/name.js
@@ -1,100 +1,121 @@
-var pullname = function(i){
+var pull3star = function(i){
 	switch(Math.floor(Math.random()*48+1)){
 		case 1 : 
- 			namearray[i]='包睿'; break;
+ 			namei='包睿'; break;
 		case 2 : 
- 			namearray[i]='陈瑜'; break;
+ 			namei='陈瑜'; break;
 		case 3 : 
- 			namearray[i]='段玉'; break;
+ 			namei='段玉'; break;
 		case 4 : 
- 			namearray[i]='姜珍怡'; break;
+ 			namei='姜珍怡'; break;
 		case 5 : 
- 			namearray[i]='林思雨'; break;
+ 			namei='林思雨'; break;
 		case 6 : 
- 			namearray[i]='任冉'; break;
+ 			namei='任冉'; break;
 		case 7 : 
- 			namearray[i]='王瑞泽'; break;
+ 			namei='王润泽'; break;
 		case 8 : 
- 			namearray[i]='校睿'; break;
+ 			namei='校睿'; break;
 		case 9 : 
- 			namearray[i]='许昕奕'; break;
+ 			namei='许昕奕'; break;
 		case 10 : 
- 			namearray[i]='张润达'; break;
+ 			namei='张润达'; break;
 		case 11 : 
- 			namearray[i]='陈超'; break;
+ 			namei='陈超'; break;
 		case 12 : 
- 			namearray[i]='戴成浩'; break;
+ 			namei='戴成浩'; break;
 		case 13 : 
- 			namearray[i]='范睿涵'; break;
+ 			namei='范睿涵'; break;
 		case 14 : 
- 			namearray[i]='金定一'; break;
+ 			namei='金定一'; break;
 		case 15 : 
- 			namearray[i]='刘阳'; break;
+ 			namei='刘阳'; break;
 		case 16 : 
- 			namearray[i]='孙国亮'; break;
+ 			namei='孙国亮'; break;
 		case 17 : 
- 			namearray[i]='王劭谦'; break;
+ 			namei='王劭谦'; break;
 		case 18 : 
- 			namearray[i]='徐耀林'; break;
+ 			namei='徐耀林'; break;
 		case 19 : 
- 			namearray[i]='杨世雨'; break;
+ 			namei='杨世雨'; break;
 		case 20 : 
- 			namearray[i]='赵晨曦'; break;
+ 			namei='赵晨曦'; break;
 		case 21 : 
- 			namearray[i]='陈恩东'; break;
+ 			namei='陈恩东'; break;
 		case 22 : 
- 			namearray[i]='戴祺轩'; break;
+ 			namei='戴祺轩'; break;
 		case 23 : 
- 			namearray[i]='韩欣睿'; break;
+ 			namei='韩欣睿'; break;
 		case 24 : 
- 			namearray[i]='孔令坤'; break;
+ 			namei='孔令坤'; break;
 		case 25 : 
- 			namearray[i]='吕妍'; break;
+ 			namei='吕妍'; break;
 		case 26 : 
- 			namearray[i]='孙亦陈'; break;
+ 			namei='孙亦陈'; break;
 		case 27 : 
- 			namearray[i]='王智梁'; break;
+ 			namei='王智梁'; break;
 		case 28 : 
- 			namearray[i]='徐英赫'; break;
+ 			namei='徐英赫'; break;
 		case 29 : 
- 			namearray[i]='姚曼文'; break;
+ 			namei='姚曼文'; break;
 		case 30 : 
- 			namearray[i]='周童'; break;
+ 			namei='周童'; break;
 		case 31 : 
- 			namearray[i]='陈杰'; break;
+ 			namei='陈杰'; break;
 		case 32 : 
- 			namearray[i]='邓岚予'; break;
+ 			namei='邓岚予'; break;
 		case 33 : 
- 			namearray[i]='洪一诺'; break;
+ 			namei='洪一诺'; break;
 		case 34 : 
- 			namearray[i]='李博仪'; break;
+ 			namei='李博仪'; break;
 		case 35 : 
- 			namearray[i]='梅之谦'; break;
+ 			namei='梅之谦'; break;
 		case 36 : 
- 			namearray[i]='吴双'; break;
+ 			namei='吴双'; break;
 		case 37 : 
- 			namearray[i]='徐子寒'; break;
+ 			namei='徐子寒'; break;
 		case 38 : 
- 			namearray[i]='殷雨婷'; break;
+ 			namei='殷雨婷'; break;
 		case 39 : 
- 			namearray[i]='周星如'; break;
+ 			namei='周星如'; break;
 		case 40 : 
- 			namearray[i]='陈优'; break;
+ 			namei='陈优'; break;
 		case 41 : 
- 			namearray[i]='窦悦然'; break;
+ 			namei='窦悦然'; break;
 		case 42 : 
- 			namearray[i]='胡恒'; break;
+ 			namei='胡恒'; break;
 		case 43 : 
- 			namearray[i]='李一豪'; break;
+ 			namei='李一豪'; break;
 		case 44 : 
- 			namearray[i]='闵忠奇'; break;
+ 			namei='闵忠奇'; break;
 		case 45 : 
- 			namearray[i]='王鹏'; break;
+ 			namei='王鹏'; break;
 		case 46 : 
- 			namearray[i]='肖文涛'; break;
+ 			namei='肖文涛'; break;
 		case 47 : 
- 			namearray[i]='许靖坤'; break;
+ 			namei='许靖坤'; break;
 		case 48 : 
- 			namearray[i]='张一然'; break;
+ 			namei='张一然'; break;
 	};
+	return namei;
 };
+
+var pull4star = function(i){
+	switch(Math.floor(Math.random()*1+1)){
+		case 1 : 
+ 			namei='徐耀林'; break;
+	}
+	return namei;
+};
+
+var pull5star = function(i){
+	switch(Math.floor(Math.random()*3+1)){
+		case 1 : 
+ 			namei='余飞'; break;
+		case 2 : 
+ 			namei='李娜'; break;
+		case 3 : 
+ 			namei='严玲'; break;
+	}
+	return namei;
+}
